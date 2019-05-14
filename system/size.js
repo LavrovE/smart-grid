f1b0d1d4d63d1c28dc593be5b458aeca9151fdf1
Eugene <1lavrov1@ukr.net>
2019-05-14 10:43:11 +0300

    test flex for media queries 1

--- system/size.js
+++ system/size.js
@@ -26,7 +26,8 @@ class Size{
             style += `{{var}}val{{=}}100% / {{var}}columns * {{var}}n{{;}}\n`;
 
             style += this.resources.styles.objToCallMedia(this.postfix, {
-                width: `{{i}}calc({{string-var}}val{{/string-var}} - {{string-var}}offset${mediaPostfix}{{/string-var}}){{/i}}`
+                width: `{{i}}calc({{string-var}}val{{/string-var}} - {{string-var}}offset${mediaPostfix}{{/string-var}}){{/i}}`,
+                'flex-basis': `{{i}}calc({{string-var}}val{{/string-var}} - {{string-var}}offset${mediaPostfix}{{/string-var}}){{/i}}`
             });
         }
 
