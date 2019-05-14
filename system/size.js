59c3bb6d6cfb1871774e2991b9e1a4142a57424a
Eugene <1lavrov1@ukr.net>
2019-05-14 10:34:30 +0300

    test flex for media queries 1

--- system/size.js
+++ system/size.js
@@ -1,11 +1,11 @@
 class Size{
-    
+
     constructor(resources, name, postfix = '') {
         this.resources = resources;
         this.name = postfix === '' ? name : name + '-' + postfix;
         this.postfix = postfix;
     }
-    
+
     render(){
         let style = '';
         let mediaPostfix = this.postfix === '' ? '' : '_' + this.postfix;
@@ -17,14 +17,14 @@ class Size{
         }
         else if(this.resources.settings.detailedCalc){
             style += `{{var}}val{{=}}{{i}}calc(100% / {{string-var}}columns{{/string-var}} * {{string-var}}n{{/string-var}} - {{string-var}}offset${mediaPostfix}{{/string-var}}){{/i}}{{;}}\n`;
-            
+
             style += this.resources.styles.objToCallMedia(this.postfix, {
                 width: '{{var}}val'
             });
         }
         else{
             style += `{{var}}val{{=}}100% / {{var}}columns * {{var}}n{{;}}\n`;
-            
+
             style += this.resources.styles.objToCallMedia(this.postfix, {
                 width: `{{i}}calc({{string-var}}val{{/string-var}} - {{string-var}}offset${mediaPostfix}{{/string-var}}){{/i}}`
             });
